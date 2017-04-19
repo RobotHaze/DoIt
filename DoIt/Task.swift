4fff452aae800ad670ed2e477ececa1ef5321475
Jordan Matthews <hayleywoodard@Hayleys-MacBook-Pro.local>
2017-04-19 16:17:40 -0400

    Table View and Task Class created

---
+++ DoIt/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Hayley Woodard on 4/19/17.
+//  Copyright © 2017 Jordan Matthews. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    
+    var name = ""
+    var important = false
+    
+}
