07c2ffde7e335c0712d317c16a8467b047bde3ec
Jordan Matthews <hayleywoodard@Hayleys-MacBook-Pro.local>
2017-04-24 17:44:06 -0400

    It’s working with CoreData!

--- DoIt/CompleteTaskViewController.swift
+++ DoIt/CompleteTaskViewController.swift
@@ -12,19 +12,18 @@ class CompleteTaskViewController: UIViewController {
     
     
     @IBOutlet weak var taskLabel: UILabel!
-    var task = Task()
-    var previousVC = TasksViewController()
+    var task : Task? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if task.important {
+        if task!.important {
             
-            taskLabel.text = "❗️\(task.name)"
+            taskLabel.text = "❗️\(task!.name!)"
             
         } else {
             
-            taskLabel.text = task.name
+            taskLabel.text = task!.name!
             
         }
         
@@ -32,9 +31,10 @@ class CompleteTaskViewController: UIViewController {
     
     
     @IBAction func completeTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         
-        previousVC.tasks.remove(at: previousVC.selectedIndex)
-        previousVC.tableView.reloadData()
+        context.delete(task!)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
         navigationController!.popViewController(animated: true)
     }
     
--- DoIt/CreateTaskViewController.swift
+++ DoIt/CreateTaskViewController.swift
@@ -13,8 +13,6 @@ class CreateTaskViewController: UIViewController {
     @IBOutlet weak var importantSwitch: UISwitch!
     @IBOutlet weak var taskNameTextField: UITextField!
     
-    var previousVC = TasksViewController()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,15 +24,17 @@ class CreateTaskViewController: UIViewController {
         
         //Create a Task from the outlet information
         
-        let task = Task()
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let task = Task(context: context)
         
         task.name = taskNameTextField.text!
         task.important = importantSwitch.isOn
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
         
-        //Add new task to array in previous viewController
         
-        previousVC.tasks.append(task)
-        previousVC.tableView.reloadData()
+        // Pop back
+        
         navigationController!.popViewController(animated: true)
     }
 
